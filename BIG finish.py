80edf55e3b862db83f71420268d8700371e476c3
OOkBuddy <44881762+OOkBuddy@users.noreply.github.com>
2018-11-08 16:55:39 -0500

    Demo

---
+++ BIG finish.py
@@ -0,0 +1,10 @@
+# Trey , November 6th, Bootcamp 1
+print ('what is your hieght')
+height = input ()
+print(height)
+Friends =['EJ','Johnny','Bryan']
+for Friends in Friends:
+    print(Friends)
+if height == '55':
+    print('finished')
+    
